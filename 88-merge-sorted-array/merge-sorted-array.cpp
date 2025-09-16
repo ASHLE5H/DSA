f8a7c71dd52053e86d7dcac5bcda1692d99dbc27
Ashlesh D Hegde <ashlesh208@gmail.com>
2025-09-16 09:18:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (8.86%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -5,10 +5,6 @@ public:
         int i = m-1;
         int j = n-1;
 
-        if(m==0){
-            nums1 = nums2;
-        }
-
         while(i>=0 && j>=0){
 
             if(nums1[i] < nums2[j]){
