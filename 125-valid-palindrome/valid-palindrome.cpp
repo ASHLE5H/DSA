16fc1f46e8bb5e7f224652a3f4917f5e820ac148
Ashlesh D Hegde <ashlesh208@gmail.com>
2025-09-19 21:49:34 +0530

    Time: 3 ms (31.58%) | Memory: 10.3 MB (27.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,31 +1,6 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-    //     for(auto &ch : s){              //using & or reference so the actual value in s change
-    //         ch = tolower(ch);
-    //     }
-
-    //     // if(isalnum(s) == isalnum(reverse(s.begin(),s.end()))){
-    //     //     return true
-    //     // }
-
-    //     string str = "";
-    //     for(auto ch : s){
-    //         if(isalnum(ch)){
-    //         str = str + ch;
-    //         }
-    //     }
-
-    //     s = str;
-    //     reverse(s.begin(),s.end());      //reversing the string
-    
-
-
-    // if(str == s){
-    //     return true;
-    // }
-    // return false;
 
 
     string str = "";
