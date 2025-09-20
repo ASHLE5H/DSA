1541094e9ed3adc58fc9851526cbe1c1aa7a11f6
Ashlesh D Hegde <ashlesh208@gmail.com>
2025-09-20 11:56:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (36.52%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     string removeOccurrences(string s, string part) {
         
-        while(s.find(part) < s.length()){
+        while(s.find(part) < s.length()){       //s.find(part) will return the first index 
+                                                // of the s were it matches part
             int i = s.find(part);
-            s.erase(i,part.length());
+            s.erase(i,part.length());           //s.erase() will take start index and end + 1 index to
+                                                //erase it from s
         }
 
         return s;
