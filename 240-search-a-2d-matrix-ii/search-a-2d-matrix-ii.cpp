8d951903d6632cbd6168009ea85313bddc5b3477
Ashlesh D Hegde <ashlesh208@gmail.com>
2025-09-25 17:17:40 +0530

    Time: 59 ms (31.31%) | Memory: 18.7 MB (67.95%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -3,31 +3,50 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
         int m = matrix.size();
-       
+        int n = matrix[0].size();       
+        int st = 0;
+        int end = n-1;
 
-        for(int i=0;i<m;i++){
-            int n = matrix[i].size();
-             int startcol = 0;
-            int endcol = n-1;
-            if(target>= matrix[i][0] && target<= matrix[i][n-1]){
-                while(startcol<=endcol){
-                
-                int mid = startcol + (endcol-startcol)/2;
+        while(st<m && end>=0){
 
-                if(matrix[i][mid] == target){
-                    return true;
-                }
-                else if(matrix[i][mid] < target){
-                    startcol = mid +1;
-                }
-                else{
-                    endcol = mid -1;
-                    
-                }
+            if(target == matrix[st][end]){
+                return true;
             }
+            else if(target > matrix[st][end]){
+                st++;
+            }
+            else{
+                end--;
             }
         }
+
+        
         return false;
         
     }
 };
+
+
+
+// for(int i=0;i<m;i++){
+//             int n = matrix[i].size();
+//              int startcol = 0;
+//             int endcol = n-1;
+//             if(target>= matrix[i][0] && target<= matrix[i][n-1]){
+//                 while(startcol<=endcol){
+                
+//                 int mid = startcol + (endcol-startcol)/2;
+
+//                 if(matrix[i][mid] == target){
+//                     return true;
+//                 }
+//                 else if(matrix[i][mid] < target){
+//                     startcol = mid +1;
+//                 }
+//                 else{
+//                     endcol = mid -1;
+                    
+//                 }
+//             }
+//             }
+//         }
