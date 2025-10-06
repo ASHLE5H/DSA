6b598ff6b94b6d24a76dbc33db55f8c2e73314aa
Ashlesh D Hegde <ashlesh208@gmail.com>
2025-10-07 01:37:01 +0530

    Time: 38 ms (79.28%) | Memory: 46.9 MB (33.00%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,26 +2,61 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         
-        vector<int> ans;
+        
         int n = nums.size();
+        vector<int> sum(n , 0);
+
         int count=0;
-
-
-        for(int i=0;i<n;i++){
-            int sum = nums[i];
-            int a = i+1;
-            if(sum == k){
-                    count++;
-                }
-            while(a<n){
-                
-                sum = sum + nums[a];
-                if(sum == k){
-                    count++;
-                }
-                a++;
-            }
+        sum[0] = nums[0];
+        for(int i=1;i<n;i++){
+            sum[i] = sum[i-1] + nums[i];
         }
+
+        unordered_map<int , int> map;
+        for(int i=0;i<n;i++){
+            if(sum[i] == k){
+                count++;
+            }
+
+            int x = sum[i] - k;
+            if(map.find(x) != map.end()){
+                count = count + map[x];
+            }
+
+            if(map.find(sum[i]) == map.end()){
+                map[sum[i]] = 0;
+            }
+            map[sum[i]]++;
+        }
+        
         return count;
+        
+        
+        
+        
+        
+        
+        
+        // vector<int> ans;
+        // int n = nums.size();
+        // int count=0;
+
+
+        // for(int i=0;i<n;i++){
+        //     int sum = nums[i];
+        //     int a = i+1;
+        //     if(sum == k){
+        //             count++;
+        //         }
+        //     while(a<n){
+                
+        //         sum = sum + nums[a];
+        //         if(sum == k){
+        //             count++;
+        //         }
+        //         a++;
+        //     }
+        // }
+        // return count;
     }
 };
