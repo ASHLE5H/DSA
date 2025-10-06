cf08efecebc43eba2b11c0bb57c9955793f25804
Ashlesh D Hegde <ashlesh208@gmail.com>
2025-10-07 02:01:31 +0530

    Time: 39 ms (76.66%) | Memory: 46.7 MB (39.47%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,60 +2,34 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         
-        
-       int n = nums.size();
-       vector<int> sum(n , 0);
-       sum[0] = nums[0];
-       for(int i=1;i<n;i++){
+       
+        int n = nums.size();
+        vector<int> sum(n , 0);
+
+        int count=0;
+        sum[0] = nums[0];
+        for(int i=1;i<n;i++){
             sum[i] = sum[i-1] + nums[i];
-       }
-       
-       unordered_map <int,int> map;
-       int count=0;
-       for(int i=0;i<n;i++){
-        if(sum[i] == k){
-            count++;
-        }
-        int x = sum[i] - k;
-        if(map.find(x) != map.end()){
-            count = count + map[x];
         }
 
-        if(map.find(sum[i]) == map.end()){
-            map[sum[i]] = 0;
+        unordered_map<int , int> map;
+        for(int i=0;i<n;i++){
+            if(sum[i] == k){
+                count++;
+            }
+
+            int x = sum[i] - k;
+            if(map.find(x) != map.end()){
+                count = count + map[x];
+            }
+
+            if(map.find(sum[i]) == map.end()){
+                map[sum[i]] = 0;
+            }
+            map[sum[i]]++;
         }
-        map[sum[i]]++;
-       }
-       return count;
-       
-       
-        // int n = nums.size();
-        // vector<int> sum(n , 0);
-
-        // int count=0;
-        // sum[0] = nums[0];
-        // for(int i=1;i<n;i++){
-        //     sum[i] = sum[i-1] + nums[i];
-        // }
-
-        // unordered_map<int , int> map;
-        // for(int i=0;i<n;i++){
-        //     if(sum[i] == k){
-        //         count++;
-        //     }
-
-        //     int x = sum[i] - k;
-        //     if(map.find(x) != map.end()){
-        //         count = count + map[x];
-        //     }
-
-        //     if(map.find(sum[i]) == map.end()){
-        //         map[sum[i]] = 0;
-        //     }
-        //     map[sum[i]]++;
-        // }
         
-        // return count;
+        return count;
         
         
         
