abd93a07b95b93621332bfa9aa79b98870e493a3
Ashlesh D Hegde <ashlesh208@gmail.com>
2025-10-06 19:39:07 +0530

    Time: 23 ms (52.79%) | Memory: 17.6 MB (17.59%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -7,25 +7,36 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n-3;i++){
-            for(int j=i+1;j<n-2;j++){
-                int k = j+1;
-                int d = n-1;
-                while(k<d){
-                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[d];
-
-                    if(sum>target) d--;
-                    else if(sum<target) k++;
-                    else{
-                        vector<int> v = {nums[i], nums[j],nums[k],nums[d]};
-                        if(set.find(v) == set.end()){
-                            set.insert(v);
-                            ans.push_back(v);
-                        }
-                        k++;
-                        d--;
-                    }
-                }
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
             }
+            else{
+                for(int j=i+1;j<n-2;j++){
+                    if(j>i+1 && nums[j] == nums[j-1]){
+                        continue;
+                    }
+                    else{
+                        int k = j+1;
+                        int d = n-1;
+                        while(k<d){
+                            long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[d];
+
+                            if(sum>target) d--;
+                            else if(sum<target) k++;
+                            else{
+                                vector<int> v = {nums[i], nums[j],nums[k],nums[d]};
+                                if(set.find(v) == set.end()){
+                                    set.insert(v);
+                                    ans.push_back(v);
+                                }
+                                k++;
+                                d--;
+                            }
+                        }
+                    }
+                
+                }
+            } 
         }
         return ans;
     }
