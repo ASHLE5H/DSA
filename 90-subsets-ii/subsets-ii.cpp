4d2c8ac4c4a9f9bd0b62e8f2a722b4d13c939819
Ashlesh D Hegde <ashlesh208@gmail.com>
2025-10-07 22:23:13 +0530

    Time: 3 ms (34.53%) | Memory: 11 MB (29.45%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void ss(vector<int>& nums , vector<vector<int>>& ans , int i , vector<int>& curr , set<vector<int>> &set){
+
+        if(i == nums.size()){
+            if(set.find(curr) == set.end()){
+                set.insert(curr);
+                ans.push_back(curr);
+            }
+            return;
+        }
+
+        curr.push_back(nums[i]);
+        ss(nums , ans , i+1 , curr , set);
+
+        curr.pop_back();
+        ss(nums , ans , i+1 , curr , set);
+
+
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        set<vector<int>> set;
+        sort(nums.begin() , nums.end());
+        ss(nums , ans , 0 , curr ,set);
+        return ans;
+        
+    }
+};
