c1e146d4058129af4ead763cf74e3b56e88d547f
Ashlesh D Hegde <ashlesh208@gmail.com>
2025-11-05 00:38:13 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (53.65%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void helper(vector<int>& candidates, int target ,vector<int>& current , vector<vector<int>>& result , int start ){
+
+        if(target == 0){
+            result.push_back(current);
+            return;
+        }
+
+        if(target < 0) return;
+
+        
+       for(int i=start;i<candidates.size();i++){
+         current.push_back(candidates[i] );
+       
+        helper(candidates , target - candidates[i] , current , result , i);
+        
+        current.pop_back();
+       }
+
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+
+            vector<int> current;
+            vector<vector<int>> result;
+            helper( candidates , target , current , result , 0);
+
+            return result;
+
+    }
+};
